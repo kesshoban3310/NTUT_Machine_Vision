e675d022aa10e9ec951e6901be4bbec481fa0dd0
NYKD-054 <nick930212@gmail.com>
2025-03-17 14:32:15 +0800

    Finish problem 1

--- 111590004_hw1.py
+++ 111590004_hw1.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
 import os
-
+from collections import Counter
+from scipy.spatial import KDTree
 def np2img(nparr,filename,folder): # Save numpy array to img
     path = os.path.join(folder,filename)
     cv2.imwrite(path,nparr)
@@ -20,6 +21,35 @@ def rgb2gray(img): # problem 1-1
 def gray2bin(img,threshold = 128): # problem 1-2
     bin_arr = np.where(img >= threshold,255,0).astype(np.uint8)
     return bin_arr
+def img2idximg(img): # problem 1-3
+    h, w, _ = img.shape
+    pixel = img.reshape(-1, 3)
+
+    # 將 RGB 轉 tuple
+    pixel_tuples = [tuple(rgb) for rgb in pixel]
+    color_count = Counter(pixel_tuples)
+    adv_32 = []
+    fre_color = color_count.most_common()
+    gz = len(fre_color) // 32
+
+    # 取出現頻率最高跟最低，共32種顏色
+    for i in range(32):
+        start = i * gz
+        end = (i + 1) * gz if i != 31 else len(fre_color)  # 最後一群全包
+        group = fre_color[start:end]
+        group_colors = np.array([color[0] for color in group])  # 只取顏色 (不取頻率)
+        avg_color = np.mean(group_colors, axis=0)  # 計算每群顏色的均值
+        adv_32.append(avg_color)
+
+    color_tree = KDTree(adv_32)
+    _,indices = color_tree.query(pixel)
+    adv_32_array = np.array(adv_32)
+
+
+    ans = adv_32_array[indices].reshape(h, w, 3).astype(np.uint8)
+
+    return ans
+
 
 
 def main():
@@ -35,17 +65,19 @@ def main():
     bin_img1 = gray2bin(gray_img1)
     bin_img2 = gray2bin(gray_img2)
     bin_img3 = gray2bin(gray_img3)
-
-
-    color_plate = [(int(i), int(i), int(i)) for i in np.linspace(0, 255, 32)]
-    print(color_plate)
+    idx_img1 = img2idximg(img1)
+    idx_img2 = img2idximg(img2)
+    idx_img3 = img2idximg(img3)
 
     np2img(gray_img1,'img1_q1-1.jpg' , folder)
-    np2img(gray_img2,'img2_q1-1 .jpg' , folder)
+    np2img(gray_img2,'img2_q1-1.jpg' , folder)
     np2img(gray_img3,'img3_q1-1.jpg' , folder)
     np2img(bin_img1, 'img1_q1-2.jpg', folder)
     np2img(bin_img2, 'img2_q1-2.jpg', folder)
     np2img(bin_img3, 'img3_q1-2.jpg', folder)
+    np2img(idx_img1, 'img1_q1-3.jpg', folder)
+    np2img(idx_img2, 'img2_q1-3.jpg', folder)
+    np2img(idx_img3, 'img3_q1-3.jpg', folder)
 
 
 if __name__ == "__main__":
