c72b306282b5aa4f3377c5f9c96df2c573f37659
arod571431 <nick930212@gmail.com>
2025-04-09 11:35:41 +0800

    Done Problem 1

--- hw2/111590004_hw2.py
+++ hw2/111590004_hw2.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
+from PIL import Image
 
 matplotlib.use('TKAgg')
 
@@ -10,9 +11,17 @@ four_mask = [[-1, 0], [0, -1]]
 
 eight_mask = [[-1, 0], [0, -1], [-1, -1]]
 
+
+def np2img(nparr, filename, folder='result_img'):  # Save numpy array to img
+    path = os.path.join(folder, filename)
+    cv2.imwrite(path, nparr)
+
+
 def gen_palette():
-    ans = np.random.randint(0, 256, (256, 3), dtype=np.uint8)
+    ans = np.random.randint(0, 256, (31, 3), dtype=np.uint8)
+    ans = np.vstack(([0, 0, 0], ans))
     return ans
+
 
 def showimg(window_name, img):  # Show Img
     cv2.imshow(window_name, img)
@@ -54,8 +63,8 @@ def gray2his(img):
     plt.show()
 
 
-def imgexpand(img,time = 1):
-    if(time == 0):
+def imgexpand(img, time=1):
+    if (time == 0):
         return img
     n, m = len(img), len(img[0])
     ans = np.zeros((n, m), dtype=np.uint8)
@@ -77,12 +86,12 @@ def imgexpand(img,time = 1):
                 if (IsOne):
                     ans[i][j] = 255
                     continue
-    ans = imgexpand(ans, time-1)
+    ans = imgexpand(ans, time - 1)
     return ans
 
 
-def imgshrink(img,time = 1):
-    if(time == 0):
+def imgshrink(img, time=1):
+    if (time == 0):
         return img
     n, m = len(img), len(img[0])
     ans = np.zeros((n, m), dtype=np.uint8)
@@ -102,70 +111,103 @@ def imgshrink(img,time = 1):
                         IsZero = True
                 if (not IsZero):
                     ans[i][j] = 255
-    ans = imgshrink(ans,time-1)
+    ans = imgshrink(ans, time - 1)
     return ans
 
-def imgdraw(img,palette):
-    n,m = len(img), len(img[0])
-    img = img % 256
-    ans = palette[img]
+
+def imgdraw(img, palette):
+    n, m = len(img), len(img[0])
+    img = img % 32
+    ans = palette[img].astype(np.uint8)
+
     return ans
+
+
+def find(x, parent):
+    while parent[x] != x:
+        parent[x] = parent[parent[x]]  # Path compression
+        x = parent[x]
+    return x
+
+
+def union(x, y, parent):
+    x_root = find(x, parent)
+    y_root = find(y, parent)
+    if x_root != y_root:
+        # Always keep the smaller label
+        parent[max(x_root, y_root)] = min(x_root, y_root)
 
 
 def imglabel(img, mask):
     img = img.astype(np.uint16)
-    label, real_lab = 1, 0
-    n, m = len(img), len(img[0])
+    label = 1
+    n, m = img.shape
+    parent = [i for i in range(n * m + 1)]  # 初始化 Union-Find 陣列
 
     for i in range(n):
         for j in range(m):
-            if (img[i][j] == 0):
+            if img[i][j] == 0:
                 continue
             neighbor = []
-            for k in mask:
-                nx, ny = i + k[0], j + k[1]
-                if (nx < 0 or ny < 0 or nx >= n or ny >= m):
-                    continue
-                if (img[nx][ny] == 0):
-                    continue
-                neighbor.append(img[nx][ny])
-            if (len(neighbor) == 0):
-                label += 1
+            for dx, dy in mask:
+                nx, ny = i + dx, j + dy
+                if 0 <= nx < n and 0 <= ny < m and img[nx][ny] != 0:
+                    neighbor.append(img[nx][ny])
+            if len(neighbor) == 0:
                 img[i][j] = label
-            elif (len(neighbor) == 1):
-                img[i][j] = neighbor[0]
+                label += 1
             else:
-                is_same = True
-                lab = neighbor[0]
-                for k in range(1, len(neighbor)):
-                    if (neighbor[k] != neighbor[k - 1]):
-                        is_same = False
-                    lab = min(lab, neighbor[k])
-                if (is_same):
-                    img[i][j] = neighbor[0]
-                else:
-                    img[i][j] = lab
-                    real_lab = max(lab, real_lab)
+                min_label = min(neighbor)
+                img[i][j] = min_label
+                for nb in neighbor:
+                    if nb != min_label:
+                        union(min_label, nb, parent)
+
+    # 第二遍：將所有 pixel 的 label 縮減為其代表
+    label_map = {}
+    new_label = 1
     for i in range(n):
         for j in range(m):
-            if (img[i][j] == 0):
-                continue
+            if img[i][j] != 0:
+                root = find(img[i][j], parent)
+                if root not in label_map:
+                    label_map[root] = new_label
+                    new_label += 1
+                img[i][j] = label_map[root]
 
-    print(label, real_lab)
+    print(new_label - 1)
     return img
 
 
+def Objcounting(img):
+    externel_point = [
+        [[0, 0], [0, 1]],
+        [[0, 0], [1, 0]],
+        [[0, 1], [0, 0]],
+        [[1, 0], [0, 0]],
+    ]
+    internel_point = [
+        [[1, 1], [1, 0]],
+        [[1, 1], [0, 1]],
+        [[1, 0], [1, 1]],
+        [[0, 1], [1, 1]],
+    ]
+    e, i = 0, 0
+    n, m = len(img), len(img[0])
+    for i in range(n - 1):
+        for j in range(m - 1):
+            a = [[img[i][j], img[i][j + 1]], [img[i + 1][j], img[i + 1][j + 1]]]
+    return e,i
+
 def main():
     input_dir = "test_img/"
-    output_dir = "result_img/"
     input_file = ["img1", "img2", "img3"]
     threshold = [227, 254, 254]
-    expand_time = [1,2,2]
-    shrink_time = [1,1,1]
+    expand_time = [1, 2, 2]
+    shrink_time = [1, 1, 1]
     expand_first = [True, False, False]
     output_file = ["_4", "_8"]
     color_palette = gen_palette()
-    print(len(color_palette))
     file_format = ".jpg"
     os.makedirs("result_img", exist_ok=True)
     for i in range(len(input_file)):
@@ -174,20 +216,25 @@ def main():
         np_img = img2array(img)
         gray_img = img2grayscale(np_img)
         bin_img = gray2bin(gray_img, threshold[i])
-        if(expand_first[i]):
+        if (expand_first[i]):
             process_img = imgshrink(bin_img, shrink_time[i])
             process_img = imgexpand(process_img, expand_time[i])
         else:
             process_img = imgshrink(bin_img, shrink_time[i])
             process_img = imgexpand(process_img, expand_time[i])
 
-        # print("Labeling Using bin_img: ",end="")
-        # a = imglabel(bin_img[:],four_mask)
-        print("Labeling Using Process_img: ", end="")
-        label_img_4 = imglabel(process_img[:],four_mask)
-        color_img_4 = imgdraw(label_img_4,color_palette)
-
+        showimg(input_file[i], process_img)
+        print("Labeling Using Process_img by 4-neighbor mask: ", end="")
+        label_img_4 = imglabel(process_img[:], four_mask)
+        color_img_4 = imgdraw(label_img_4, color_palette)
         showimg(input_file[i], color_img_4)
+        print("Labeling Using Process_img by 8-neighbor mask: ", end="")
+        label_img_8 = imglabel(process_img[:], eight_mask)
+        color_img_8 = imgdraw(label_img_8, color_palette)
+        showimg(input_file[i], color_img_8)
+
+        np2img(color_img_4, input_file[i] + output_file[0] + file_format)
+        np2img(color_img_8, input_file[i] + output_file[1] + file_format)
 
 
 if __name__ == "__main__":
